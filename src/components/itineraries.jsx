cf365c9e752e01c914b1f06a853c89bd40f016a7
diane-ch <diane.chounlamountry@gmail.com>
2025-03-13 12:02:28 +0000

    updates accordeon

--- src/components/itineraries.jsx
+++ src/components/itineraries.jsx
@@ -7,6 +7,7 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
+import Accordion from "react-bootstrap/Accordion";
 
 const Itineraries = () => {
     return (
@@ -17,18 +18,69 @@ const Itineraries = () => {
                 </Row>
             </Container>
 
-            <Container className="d-flex justify-content-center green-rectangle">
-                    <Row className=" w-100">
+            <Container className="my-4 p-3 rounded" style={{backgroundColor:"#0b524c"}}>
+                <Row>
                     <Col>
-                        <h2 style={{ color: 'white', textAlign: 'center' }}>DUBLIN</h2>
+                        <h2 className="text-center p-3" style={{ color: '#F19EDC', fontWeight: "bold" }}>DUBLIN</h2>
                     </Col>
 
                     <Col>
-                    <h2>Suggested Itineraries</h2>
+                        <h2 className="text-center p-3" style={{ color: '#F19EDC', fontSize: "1.8rem" }}>Suggested itineraries</h2>
+
+                        <Accordion>
+                            <Accordion.Item eventKey="0">
+                            <Accordion.Header>Adventurous</Accordion.Header>
+                            <Accordion.Body>Preview of the itinerary.
+                            <div className="mt-auto d-flex justify-content-end">
+                                <Button style={{ backgroundColor: "#88e097", border: "none", color: "#000" }}>
+                                    Go
+                                </Button>
+                            </div>
+                            </Accordion.Body>
+                            </Accordion.Item>
+                            <Accordion.Item eventKey="1">
+                            <Accordion.Header>Touristic</Accordion.Header>
+                            <Accordion.Body>Preview of the itinerary.
+                            <div className="mt-auto d-flex justify-content-end">
+                                <Button style={{ backgroundColor: "#88e097", border: "none", color: "#000" }}>
+                                    Go
+                                </Button>
+                            </div>
+                            </Accordion.Body>
+                            </Accordion.Item>
+                            <Accordion.Item eventKey="2">
+                            <Accordion.Header>Lazy</Accordion.Header>
+                            <Accordion.Body>Preview of the itinerary.
+                            <div className="mt-auto d-flex justify-content-end">
+                                <Button style={{ backgroundColor: "#88e097", border: "none", color: "#000" }}>
+                                    Go
+                                </Button>
+                            </div>
+                            </Accordion.Body>
+                            </Accordion.Item>
+                            <Accordion.Item eventKey="3">
+                            <Accordion.Header>Original</Accordion.Header>
+                            <Accordion.Body>
+                                Preview of the itinerary.
+                                <p className="mt-2">
+                                The photos on the side can either be associated to each itinerary or be pictures of the city (same for all previews).
+                                </p>
+                                <div className="mt-auto d-flex justify-content-end">
+                                <Button style={{ backgroundColor: "#88e097", border: "none", color: "#000" }}>
+                                    Go
+                                </Button>
+                            </div>
+                            </Accordion.Body>
+                            </Accordion.Item>
+                        </Accordion>
+
+
+
                     </Col>
-                    </Row>
+                </Row>
             </Container>
 
+                       
         </div>
     );
 };
