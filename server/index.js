d1e1382827350188830c5d8be441963636625f31
MartinNang <53534948+MartinNang@users.noreply.github.com>
2025-03-18 15:20:42 +0000

    updated index.js to support mariadb

--- server/index.js
+++ server/index.js
@@ -5,15 +5,24 @@ import cors from "cors";
 import mariadb from "mariadb";
 
 const app = express();
-import * as itineraryController from "./controllers/itineraryController.js";
-import * as userController from "./controllers/userController.js";
+import * as itineraryController from "./server/controllers/itineraryController.js";
+import * as userController from "./server/controllers/userController.js";
 import {
   deleteItinerary,
   updateItinerary,
-} from "./services/itineraryService.js";
+} from "./server/services/itineraryService.js";
 const hostname = "nodejs_2425-cs7025-group1";
 const port = 3000;
 const project_name = "en route";
+
+// Create a connection pool
+const pool = mariadb.createPool({
+  host: "mariadb_2425-cs7025-group1",
+  user: "2425-cs7025-group1",
+  password: "jdgMm6VkTKTmhMv6",
+  database: "2425-cs7025-group1_db",
+  connectionLimit: 5,
+});
 
 // Use Morgan for logging HTTP requests
 app.use(morgan("dev"));
@@ -30,80 +39,71 @@ app.get("/", (req, res) => {
 });
 
 // return all users
-app.get("/users", (req, res) => {
-  const conn = pool.getConnection();
+app.get("/users", async (req, res) => {
+  const conn = await pool.getConnection();
   userController.getAllUsers(conn, req, res);
 });
 
 // check if user exists
-app.get("/login", (req, res) => {
-  const conn = pool.getConnection();
+app.get("/login", async (req, res) => {
+  const conn = await pool.getConnection();
   userController.findUser(conn, req, res);
 });
 
 // create new user
-app.post("/signup", (req, res) => {
-  const conn = pool.getConnection();
+app.post("/signup", async (req, res) => {
+  const conn = await pool.getConnection();
   userController.findUser(conn, req, res);
 });
 
 // update profile name
-app.put("/users/:userId/profileName", (req, res) => {
-  const conn = pool.getConnection();
+app.put("/users/:userId/profileName", async (req, res) => {
+  const conn = await pool.getConnection();
   userController.updateProfileName(conn, req, res);
 });
 
 // update profile image
-app.put("/users/:userId/profileImage", (req, res) => {
-  const conn = pool.getConnection();
+app.put("/users/:userId/profileImage", async (req, res) => {
+  const conn = await pool.getConnection();
   userController.updateProfileImage(conn, req, res);
 });
 
 // delete user
-app.delete("/users/:userId", (req, res) => {
-  const conn = pool.getConnection();
+app.delete("/users/:userId", async (req, res) => {
+  const conn = await pool.getConnection();
   userController.deleteUser(conn, req, res);
 });
 
 // return all itineraries from all users
-app.get("/itineraries", (req, res) => {
-  const conn = pool.getConnection();
+app.get("/itineraries", async (req, res) => {
+  const conn = await pool.getConnection();
   itineraryController.getAllItineraries(conn, req, res);
 });
 
 // return all itineraries from one user
-app.get("/users/:userId/itineraries", (req, res) => {
-  const conn = pool.getConnection();
+app.get("/users/:userId/itineraries", async (req, res) => {
+  const conn = await pool.getConnection();
   itineraryController.findItinerariesByUserId(conn, req, res);
 });
 
 // create new itinerary
-app.post("/itineraries", (req, res) => {
-  const conn = pool.getConnection();
+app.post("/itineraries", async (req, res) => {
+  const conn = await pool.getConnection();
   itineraryController.createItinerary(conn, req, res);
 });
 
 // update existing itinerary
-app.post("/itineraries", (req, res) => {
-  const conn = pool.getConnection();
+app.post("/itineraries", async (req, res) => {
+  const conn = await pool.getConnection();
   itineraryController.updateItinerary(conn, req, res);
 });
 
 // delete itinerary
-app.delete("/itineraries", (req, res) => {
-  const conn = pool.getConnection();
+app.delete("/itineraries", async (req, res) => {
+  const conn = await pool.getConnection();
   itineraryController.deleteItinerary(conn, req, res);
 });
 
 app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
-});
-
-// Create a connection pool
-const pool = mariadb.createPool({
-  host: "mariadb_2425-cs7025-group1",
-  user: "2425-cs7025-group1",
-  password: "jdgMm6VkTKTmhMv6",
-  database: "2425-cs7025-group1_db",
-  connectionLimit: 5,
 });
