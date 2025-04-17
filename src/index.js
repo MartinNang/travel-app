93c646ca91fa93a546d67bff430dbae3a91ab6c8
MartinNang <53534948+MartinNang@users.noreply.github.com>
2025-04-17 15:38:45 +0200

    register service worker for offline capabilities

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { HashRouter } from "react-router-dom";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 // import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -13,6 +14,8 @@ root.render(
     </HashRouter>
   </React.StrictMode>
 );
+
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
