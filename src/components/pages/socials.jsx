c9ac65cfc35869dd4de5323c04e3b133b538f55a
Eve-tcd <ebrady1@tcd.ie>
2025-04-25 00:52:04 +0100

    socials now link to a coming soon page

---
+++ src/components/pages/socials.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import { Link } from "react-router-dom";
+import AboutImg from "../../images/maps.png"
+
+const Socials = () => {
+    return (
+<>
+<div class="socials">
+<h1 >Socials coming soon..</h1>
+</div>
+</>
+    );
+};
+
+export default Socials;
